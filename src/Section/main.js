b9608b6d522cada9d997d11521eb45e21c8629ae
bintangpr <bintangpr175@gmail.com>
2022-02-04 13:20:47 +0700

    template update
    template update

---
+++ src/Section/main.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const CenterContent = styled.div`
+  width: 100%;
+  
+  background-color: #000;
+`;
+
+function main() {
+    return (
+      <div className='h-screen w-screen bg-white' >
+        <div className='bg-black w-4/5'>
+
+        </div>
+      
+      </div>
+    );
+  }
+  
+  export default main;
---
+++ src/Section/profile.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+
+function profile() {
+    return (
+      <div>
+ 
+      </div>
+    );
+  }
+  
+  export default profile;
