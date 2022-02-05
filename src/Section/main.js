3965b3d18378476f6fa5604ba23dcfa4723d4f29
bintangpr <bintangpr175@gmail.com>
2022-02-05 15:00:08 +0700

    first update

--- src/Section/main.js
+++ src/Section/main.js
@@ -1,16 +1,12 @@
-import styled from 'styled-components';
-
-const CenterContent = styled.div`
-  width: 100%;
-  
-  background-color: #000;
-`;
 
 function main() {
     return (
       <div className='h-screen w-screen bg-white' >
-        <div className='bg-black w-4/5'>
-
+        <div className=' w-4/5 mx-auto h-screen'>
+          <div className="pt-24">
+            <h1>Designer , Front-end Developer & Human Resource Ethusiast</h1>
+          </div>
+        
         </div>
       
       </div>
