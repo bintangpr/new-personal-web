882e9ac0784854b593822862bff5cb4506933347
bintangpr <bintangpr175@gmail.com>
2022-02-07 19:25:43 +0700

    add navbar + rework main section

--- src/Section/main.js
+++ src/Section/main.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import {ReactComponent as ProfilePic} from '../img/Profile.svg';
 import {ReactComponent as ArrowScroll} from '../img/ArrowScroll.svg';
-import {ReactComponent as Bubble} from '../img/Bubble.svg';
+
+import Navbar from '../component/navbar';
 
 function main() {
     return (
-      <div className='h-screen bg-white' >
-          <div className="w-4/5 mx-auto h-screen  flex flex-col justify-around">
-            <div>
-              <h1 className="text-3xl font-bold text-red text-center my-4">Designer , Front-end Developer & Human Resource Ethusiast</h1>
-              <h1 className="text-3xl text-black text-center my-4">I design & code simple and clean. I also love what i do</h1>
-            </div>            
-            <div>
-              <ProfilePic className='mx-auto'/>
+      <div className='bg-white'>
+        <Navbar />
+        <div className='flex flex-col justify-around bg-white h-screen'>
+          <div className="w-4/5 mx-auto h-screen flex justify-around" >
+            <div className='my-auto '>
+                <ProfilePic className='mx-auto scale-125 '/>  
             </div>
-            <div>
-              <ArrowScroll className='mx-auto' />
+            <div className='my-auto'>
+                <h1 className="text-xl font-medium text-black my-4">Hello. . .</h1>
+                <h1 className="text-xl font-medium text-black my-4">Welcome to My page</h1>
+                <div>
+                  <h1 className="text-xl font-medium text-black my-4">My name is</h1>
+                  <h1 className="text-xl font-medium text-black my-4">Muhammad Bintang Prabowo</h1>
+                </div>
+                <div>
+                  <h1 className="text-3xl font-bold text-red my-4">A Frontend Developer</h1>
+                </div>
+                <div>
+                  <h1 className="text-xl font-medium text-black my-4">Im from </h1>
+                  <h1 className="text-xl font-medium text-black my-4">South Tangerang, Indonesia</h1>
+                </div>
             </div>
           </div>
-          <Bubble className='absolute bottom-0 right-10'/>
+          <div>
+              <ArrowScroll className='mx-auto ' />
+            </div>
+        </div>
       </div>
     );
   }
---
+++ src/component/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function navbar() {
+    return (
+      <div className='h-16 bg-lightgreen w-full fixed' >
+         
+      </div>
+    );
+  }
+  
+  export default navbar;
