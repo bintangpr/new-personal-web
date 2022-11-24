a42946249eff1e1c9174195587611e4aff6ab8d2
bintangpr <bintangpr175@gmail.com>
2022-11-24 09:24:44 +0700

    change font size at profile

--- src/Section/profile.js
+++ src/Section/profile.js
@@ -10,7 +10,7 @@ function profile() {
         <div className="w-4/5 mx-auto">
           <div className="bg-lightgreen w-32 sm:w-56 h-3"/>
           <h1 className="my-4 text-2xl md:text-3xl font-medium text-blue">My Profile</h1>
-          <p className="text-xl font-normal">{profileText.myProfile}</p>
+          <p className="text-lg sm:text-xl font-normal">{profileText.myProfile}</p>
           <div className="my-4 block sm:flex justify-center gap-24">
             <Card name="Frontend Developer" bg="bg-blue/25" border="border-blue" img={CodeImage} techlist={techListing}/>
             <Card name="Android Developer" bg="bg-lightgreen/25" border="border-lightgreen" img={CodeImage} techlist={androidListing}/>
